be96953aba081bb24e2a05ee383be91c74ec8df8
edytatar <edytatar@gmail.com>
2021-12-29 14:01:23 -0600

    added mainPrompt function

--- server.js
+++ server.js
@@ -20,3 +20,51 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
+
+
+// Prompt when connection is created
+db.connect(err => {
+    if (err) console.error(err);
+    mainPrompt();
+});
+
+const mainPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+                name: 'options',
+                message: "What would you like to do?",
+                choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee", "Quit"],
+                loop: false,
+        },
+       
+    ])
+    .then(data => {
+        switch (data.options) {
+            case "View all departments":
+                viewAllDepartments();
+                break;
+            case "View all roles":
+                viewAllRoles();
+                break;
+            case "View all employees":
+                viewAllEmployees();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Update an employee":
+                updateEmployee();
+                break;
+            case "Quit":
+                db.end()
+                break;
+        }
+    })
+}
