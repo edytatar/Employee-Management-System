d789f7925a90355d75cc49e25368f577120dfc6b
edytatar <edytatar@gmail.com>
2021-12-29 13:29:12 -0600

    Seeded db with department and role info

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (name)
+VALUES ("Accounting"),
+       ("Customer Support"),
+       ("Data"),
+       ("Design"),
+       ("Engineering"),
+       ("Human Resources"),
+       ("IT"),
+       ("Marketing"),
+       ("People"),
+       ("Product"),
+       ("Research and Development"),
+       ("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 80000, 1),
+       ("Customer Support Specialis", 50000, 2),
+       ("Data Analyst", 85000, 3),
+       ("Junior Graphic Designer", 40000, 4),
+       ("Senior Recruiter", 95000, 5),
+       ("IT Specialist", 75000, 6),
+       ("Group Marketing Manager", 150000, 7),
+       ("Chief of Staff", 650000, 8),
+       ("Developer Advocate", 150000, 9),
+       ("Principal Researcher", 500000, 10),
+       ("Account Manager", 35000, 11);
